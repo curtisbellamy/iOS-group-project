575d631064a305ac3e83ae08e82c930878cbd7d6
Curtis Bellamy <curtisbellamy@Curtiss-Mac-mini.local>
2019-09-21 12:28:48 -0700

    Added ThirdViewController

---
+++ bigMother_iOS/bigMother_iOS/ThirdViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ThirdViewController.swift
+//  bigMother_iOS
+//
+//  Created by Curtis Bellamy on 2019-09-21.
+//  Copyright © 2019 Curtis Bellamy. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
