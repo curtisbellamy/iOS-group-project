d7176e7dca3dfffc9121941d8fecf28e5dd8963b
Curtis Bellamy <curtisbellamy@Curtiss-Mac-mini.local>
2019-09-21 10:54:45 -0700

    Initial commit

---
+++ bigMother_iOS/bigMother_iOS/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  bigMother_iOS
+//
+//  Created by Curtis Bellamy on 2019-09-21.
+//  Copyright © 2019 Curtis Bellamy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
