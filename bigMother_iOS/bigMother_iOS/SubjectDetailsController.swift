42552e25c1dce8fc6e236edca0cd77b7db261690
Yuanyuan Zhang <yuanyuanzhang1104@gmail.com>
2019-09-24 23:37:02 -0700

    Add Subject and SubjectDetails

---
+++ bigMother_iOS/bigMother_iOS/SubjectDetailsController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  bigMother_iOS
+//
+//  Created by Yuanyuan Zhang on 2019-09-24.
+//  Copyright © 2019 Curtis Bellamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = subjects[myIndex]
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
