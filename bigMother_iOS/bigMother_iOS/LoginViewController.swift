6addde7b46b2abd0a4fdd092683e583c82c25aa0
Yuanyuan Zhang <yuanyuanzhang1104@gmail.com>
2019-12-05 21:41:52 -0800

    comment out the code for user doesn't exist

--- bigMother_iOS/bigMother_iOS/LoginViewController.swift
+++ bigMother_iOS/bigMother_iOS/LoginViewController.swift
@@ -15,7 +15,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginStatusLabel: UILabel!
-    @IBOutlet weak var entryFailText: UILabel!
+//    @IBOutlet weak var entryFailText: UILabel!
     var lastKnownCoordinates : [CustomLocation] = []
     
     var ID : String = ""
@@ -90,11 +90,12 @@ class LoginViewController: UIViewController {
                 
                             
                         }
-                    } else {
-                        self.entryFailText.text = "user doesn't exist, please signup!"
-                        print(self.entryFailText.text!)
-                        
                     }
+//                    else {
+//                        self.entryFailText.text = "user doesn't exist, please signup!"
+//                        print(self.entryFailText.text!)
+//
+//                    }
                 }
             }
         }
